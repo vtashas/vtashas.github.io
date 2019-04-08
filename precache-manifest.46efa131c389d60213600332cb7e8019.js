618c5db650cdb8fe8345a104262f279a033c64ad
Vadia Tashari Sabira <vadia.sabira@yahoo.com>
2019-04-08 16:58:09 +0700

    first commit

---
+++ precache-manifest.46efa131c389d60213600332cb7e8019.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c48053ed95e28a2ec7c6e3b784b1c16",
+    "url": "/static/media/js-logo.4c48053e.png"
+  },
+  {
+    "revision": "0245f365852fa717eb44c8c34b1f39c8",
+    "url": "/static/media/html-resize.0245f365.png"
+  },
+  {
+    "revision": "bf1310ea4455d8f9bd487bceae3e848e",
+    "url": "/static/media/css-resize.bf1310ea.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "29d8b0b6e88dbd9eae35",
+    "url": "/static/js/main.daf92d67.chunk.js"
+  },
+  {
+    "revision": "a7c3c3c7a3bd9774ea9a",
+    "url": "/static/js/2.942d6197.chunk.js"
+  },
+  {
+    "revision": "29d8b0b6e88dbd9eae35",
+    "url": "/static/css/main.986b20a2.chunk.css"
+  },
+  {
+    "revision": "ff84dbbe715e26ca19b5b336ab22b395",
+    "url": "/index.html"
+  }
+];
